a23bf78760476a5d6e804d89bd810e6a04f640b4
basilis21anagno <32620316+basilis21anagno@users.noreply.github.com>
2018-01-08 18:13:14 +0200

    Update Corrector.java
    just putting in the required comments

--- Corrector.java
+++ Corrector.java
@@ -6,12 +6,12 @@ public class Corrector {
   private final String text;
   private List<String> wrongWords;
   private Dictionary dictionary;
-
+  //Creation of the constructor 
   public Corrector(String text, Dictionary dictionary) {  
     this.text = text;
     this.dictionary = dictionary;
   }
-
+  //Creating originalWords array to store words from the text given then checks if these words exist in dictionary if not adds it in wrongwordsList
   public void correctText() {
 
     wrongWords = new ArrayList<>();
