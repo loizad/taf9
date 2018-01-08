41f10cfbd492c0d83d6e4c115b06e2dbfe6855ee
dimitriosdelegkos <32551179+dimitriosdelegkos@users.noreply.github.com>
2018-01-08 21:00:17 +0200

    Update OrthoCorrect.java
    Added a part of start method also created  Corrector object and executed method correctText and printWrongWords

--- OrthoCorrect.java
+++ OrthoCorrect.java
@@ -7,6 +7,8 @@ public class OrthoCorrect {
   private Dictionary dictionary;
   private Corrector corrector;
   private Scanner consoleReader;
+ 
+  
   //This method reads the text from the console creates a Dictionary object and then checks if it is loadedSuccessfully if not throws exception 
   private void initializeApplication(String dictionaryFilePath) throws Exception {
     consoleReader = new Scanner(System.in, "windows-1253"); 
@@ -14,4 +16,44 @@ public class OrthoCorrect {
     boolean dictionaryWasLoadedSuccessfully = dictionary.loadDictionary(dictionaryFilePath); 
     if (!dictionaryWasLoadedSuccessfully) 
       throw new Exception("Αποτυχία φόρτωσης λεξικού.Η εφαρμογή θα σταματήσει");
-}
+  
+  }
+  
+  //This method actually executes the application creating a Corrector object that takes 2 parameters the text to check and the dictionary
+  //it runs correctText and pringWrongWords
+  private void start() throws IOException {
+     String inputText;
+     boolean tryAgain;
+
+
+
+
+     do {
+       System.out.println("Πρόγραμμα διόρθωσης ορθογραφίας");
+       System.out.println("Παρακαλώ εισάγετε μία φράση και πατήστε ENTER");
+
+       inputText = consoleReader.nextLine();
+       System.out.println(inputText);
+
+
+
+
+       corrector = new Corrector(inputText, dictionary);
+       corrector.correctText();
+       corrector.printWrongWords();
+
+
+       System.out.println("Θέλετε να δώσετε καινούριο κείμενο; Y/n");
+       inputText = consoleReader.nextLine();
+
+  
+  
+
+
+  
+  
+  
+  
+  
+  
+  
