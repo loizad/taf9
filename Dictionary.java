64114caeecede57ad22053a31201dfa6bfb0bfb5
kosmasdimitropoulos <33027134+kosmasdimitropoulos@users.noreply.github.com>
2018-01-08 14:56:08 +0200

    Update Dictionary.java
    Adding dictionaryToList method in Dictionary class in order to read the dictionary file open it  and append to our List each word from the dictionary file

--- Dictionary.java
+++ Dictionary.java
@@ -11,6 +11,7 @@ public class Dictionary {
   public Dictionary() {
       this.dictionaryList = new ArrayList<>();
   }
+  
   //Check if dictionary exists and if ti is loaded return true or else false //
   public boolean loadDictionary(String dictionaryFilePath) {
       dictionary = new File(dictionaryFilePath); 
@@ -24,4 +25,34 @@ public class Dictionary {
         return false;
       }
       return true;
+  }
+
+  //This method opens the dictionary file and then read it then adds each row to the List accordingly//
+  
+   private void dictionaryToList() throws IOException {
+      FileReader fileReader = null;
+      BufferedReader reader = null;
+      try {
+        fileReader = new FileReader(dictionary);
+        reader = new BufferedReader(fileReader);
+        String word = null;
+        while ((word = reader.readLine()) != null) {
+          dictionaryList.add(word); // each word is APPENDED to the list. We assume the words come IN ORDER
+        }
+      } catch (FileNotFoundException e) {
+        System.out.println("Μη αναμενόμενη συμπεριφορά! Το αρχείο του λεξικού έχει διαγραφεί από εξωτερικό παράγοντα.");
+        throw e;
+      } catch (IOException e) {
+        System.out.println("Μη αναμενόμενη συμπεριφορά! Εμφανίστηκε πρόβλημα κατά το διάβασμα του λεξικού. Παρακαλώ ελέγξτε την ορθότητά του.");
+        throw e;
+      } finally {
+        try {
+          if (reader != null) {
+            reader.close();
+          }
+        } catch (IOException e) {
+
+        }
+      }
+    }
   }
